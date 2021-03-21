b6abca903f43f9896df4e0aa7fdb1acea2a532cc
nyuta <pillowforneedles@gmail.com>
2021-03-21 17:27:56 +0200

    search

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,28 +3,43 @@ import axios from "axios";
 import { arrayHeader } from "./dataTable";
 import { UsersStyles } from "./UsersStyled";
 
+const initialChecked = { m: false, f: false };
+const initialSearch = { name: "", lastname: "", age: "" };
+
 function Users() {
   const [userData, setUserData] = useState([]);
-  const [checked, setChecked] = useState({ m: false, f: false });
+  const [checked, setChecked] = useState(initialChecked);
   const [isShow, setIsShow] = useState(true);
+  const [search, setSearch] = useState(initialSearch);
+
+  // const onHandleChange = (e) => {
+  //   // const { name, value } = e.target;
+  //   setSearch((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  //   console.log(`value`, value);
+  //   console.log(`search`, search);
+
+  //   setUserData(
+  //     userData.filter((user) =>
+  //       user.name.toLowerCase().includes(search[name].toLowerCase())
+  //     )
+  //   );
+  //   console.log(`userData`, userData);
+  // };
 
   const onHandleCheck = (e) => {
     setIsShow(false);
     const { name } = e.target;
-    // console.log(`e.target.checked`, e.target.checked);
 
-    setChecked((prevChecked) => {
-      return { ...prevChecked, [name]: !prevChecked.name };
-    });
+    setChecked((prev) => ({ ...prev, [name]: !prev.name }));
 
-    setUserData((prevData) => [
-      ...prevData.map((user) =>
+    setUserData((prev) => [
+      ...prev.map((user) =>
         name === user.sex ? { ...user, checked: !user.checked } : { ...user }
       ),
     ]);
   };
 
-  const getUsers = () => {
+  function getUsers() {
     axios
       .get("https://venbest-test.herokuapp.com/")
       .then((res) =>
@@ -36,13 +51,27 @@ function Users() {
         ])
       )
       .catch((err) => console.log(err));
-  };
+  }
+
+  useEffect(() => {
+    Object.values(search).every((value) => value === "") && getUsers();
+    // Object.values(search).some((value) => value === "") &&
+    // setUserData(
+    //   userData.filter((user) => user.toLowerCase().includes(search))
+    // );
+  }, [search]);
 
   useEffect(() => {
     !userData.length && getUsers();
   }, []);
 
-  console.log(`userData`, userData);
+  // useEffect(() => {
+  //   const results = userData.filter((user) =>
+  //     user.toLowerCase().includes(search)
+  //   );
+  //   setUserData(results);
+  //   !userData.length && getUsers();
+  // }, [search, userData]);
 
   return (
     <UsersStyles>
@@ -57,7 +86,25 @@ function Users() {
                   <input
                     type={arrHeader.type}
                     name={arrHeader.name}
+                    value={search[arrHeader.name]}
                     placeholder={arrHeader.placeholder}
+                    onChange={(e) => {
+                      const { name, value } = e.target;
+                      setSearch((prev) => ({
+                        ...prev,
+                        [name]: name === "age" ? Number(value) : value,
+                      }));
+                      // setUserData(
+                      //   userData.filter((user) =>
+                      //     // user[name] !== "age"
+                      //     //   ? user[name]
+                      //     //       .toLowerCase()
+                      //     //       .includes(search[name].toLowerCase())
+                      //     //   : user[name] === value
+                      //     console.log(search)
+                      //   )
+                      // );
+                    }}
                   />
                 ) : (
                   arrHeader.checkboxes.map((checkbox) => (
@@ -73,6 +120,8 @@ function Users() {
                 )}
               </th>
             ))}
+            {/* {console.log(`search`, search)}
+            {console.log(`userData`, userData)} */}
           </tr>
           {userData.map(
             (user) =>
--- src/components/users/dataTable.js
+++ src/components/users/dataTable.js
@@ -1,15 +1,23 @@
 export const arrayHeader = [
-  { title: "Имя", type: "text", name: "name", placeholder: "Введите имя" },
+  {
+    title: "Имя",
+    type: "text",
+    name: "name",
+    value: "",
+    placeholder: "Введите имя",
+  },
   {
     title: "Фамилия",
     type: "text",
     name: "lastname",
+    value: "",
     placeholder: "Введите фамилию",
   },
   {
     title: "Возраст",
     type: "number",
     name: "age",
+    value: "",
     placeholder: "Введите возраст",
   },
   {
